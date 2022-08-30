81369d73699c93dcd332686b548c12f747a6bc81
Adinho69 <adryann.rafael123@gmail.com>
2022-08-30 14:31:28 -0300

    add cards

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,81 +1,103 @@
 <template>
   <main>
-
     <div class="div-home">
-      <a href="/"><img class="logo-home" src="https://raw.githubusercontent.com/ilgneer/Guarani-Project/main/src/assets/images/logo1.png"></a>
+      <a href="/"
+        ><img
+          class="logo-home"
+          src="https://raw.githubusercontent.com/ilgneer/Guarani-Project/main/src/assets/images/logo1.png"
+      /></a>
     </div>
 
-
     <div class="text-top">
-      <p>GUARANI é uma Plataforma Digital que disponibiliza o Acesso Aberto às pesquisas de forma rápida e eficiente. A Política de Acesso Aberto ao Conhecimento Científico da FAPEMA foi instituída pela Portaria n° 32/2019 para garantir a transmissão do conhecimento científico e tecnológico do Maranhão à sociedade, em especial à comunidade científica, além de reforçar a responsabilidade sociocientífica regional.
+      <p>
+        GUARANI é uma Plataforma Digital que disponibiliza o Acesso Aberto às
+        pesquisas de forma rápida e eficiente. A Política de Acesso Aberto ao
+        Conhecimento Científico da FAPEMA foi instituída pela Portaria n°
+        32/2019 para garantir a transmissão do conhecimento científico e
+        tecnológico do Maranhão à sociedade, em especial à comunidade
+        científica, além de reforçar a responsabilidade sociocientífica
+        regional.
       </p>
     </div>
 
-  <v-app id="inspire">
-    <v-card
-      class="mx-auto"
-      max-width="200"
-    >
-      <v-img
-        class="white--text align-end"
-        height="100px"
-        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-      >
-        <v-card-title>Projetos</v-card-title>
-      </v-img>
+    <v-app id="inspire">
+      <div class="content">
+        <v-card class="mx-auto" max-width="200" v-for="i in 3" :key="i">
+          <v-img
+            class="white--text align-end"
+            height="100px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>Projetos</v-card-title>
+          </v-img>
 
-      <v-card-actions>
-        <v-btn
-          color="orange"
-          text
-        >
-          Ver sobre
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-app>
+          <v-card-actions>
+            <v-btn color="orange" text> Ver sobre </v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
 
+      <div class="content">
+        <v-card class="mx-auto" max-width="200" v-for="i in 3" :key="i">
+          <v-img
+            class="white--text align-end"
+            height="100px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>Projetos</v-card-title>
+          </v-img>
+
+          <v-card-actions>
+            <v-btn color="orange" text> Ver sobre </v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
+    </v-app>
   </main>
 </template>
 
 <script>
 export default {
-  name: 'HomePage'
-}
+  name: "HomePage",
+};
 </script>
 
 <style scoped>
-
-  main {
-    background-color: var(--color-background-home);
-  }
-
-  .div-home {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding-top: 20px;
+main {
+  background-color: var(--color-background-home);
 }
 
-  .logo-home {
-    width: 700px;
- }
+.div-home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 20px;
+}
 
-  .text-top p {
-    color: white;
-    margin: 13px 40px;
-    text-align: center;
-  }
+.logo-home {
+  width: 700px;
+}
 
-  .text-top:hover {
-    opacity: 0.8;
-  }
+.text-top p {
+  color: white;
+  margin: 13px 40px;
+  text-align: center;
+}
 
-  #inspire {
-    padding-top: 50px;
-    display: flex;
-    flex-direction: row;
-  }
+.text-top:hover {
+  opacity: 0.8;
+}
 
-
+#inspire {
+  padding-top: 50px;
+  display: flex;
+  flex-wrap: wrap;
+}
+.content {
+  width: 70rem;
+  flex-wrap: wrap;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
