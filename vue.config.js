c6e2a679115f316d639fc525da6b8ab1a8b0a608
Ilgner <mendesilgner@gmail.com>
2022-08-30 11:10:16 -0300

    correção bugs

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-<<<<<<< HEAD
-  transpileDependencies: [
-    'vuetify'
-  ]
-=======
+  transpileDependencies: ['vuetify'],
   transpileDependencies: true
->>>>>>> 0da036b56ee0f0a78ecc1b7797ca770f7839c11f
 })
