3fdd6ad4b6bd7e13ace1819e26f7cbdabb6064b7
Ilgner <mendesilgner@gmail.com>
2022-08-30 02:05:07 -0300

    v3

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,23 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
 
     <HeaderComp/>
     <HomePage/>
 
-=======
->>>>>>> 0da036b56ee0f0a78ecc1b7797ca770f7839c11f
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 import './styles/global.css'
 
 import HeaderComp from './components/Header.vue'
 import HomePage from './pages/Home.vue'
 
-
-=======
->>>>>>> 0da036b56ee0f0a78ecc1b7797ca770f7839c11f
-
 export default {
   name: 'App',
   components: {
-<<<<<<< HEAD
     HeaderComp,
     HomePage
   }
 }
 </script>
-=======
-
-  }
-}
-</script>
- 
->>>>>>> 0da036b56ee0f0a78ecc1b7797ca770f7839c11f
